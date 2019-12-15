87260b0fe0a44daf317f6387671df4e38ee6f65a
Daniel Friyia <dan@trainwithpush.com>
2019-12-14 19:06:24 -0500

    Service Added

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,7 @@ import 'package:flutter_starter/modules/login/login_screen.dart';
 import 'package:flutter_starter/navigation/dialogs/dialog_manager.dart';
 import 'package:flutter_starter/navigation/dialogs/global_dialog_service.dart';
 import 'package:flutter_starter/navigation/navigation/global_navigation_service_locator.dart';
+import 'package:flutter_starter/navigation/navigation/navigation_service.dart';
 import 'package:flutter_starter/navigation/routes.dart';
 import 'package:provider/provider.dart';
 
@@ -12,14 +13,17 @@ void main() => runApp(MyApp());
 class MyApp extends StatelessWidget {
   final LoginScreenBloc _bloc = LoginScreenBloc();
 
+  static final NavigationService _navigationService =
+      GlobalNavigationServiceLocator().locator<NavigationService>();
+
   MyApp() {
     GlobalDialogServiceLocator();
-    GlobalNavigationServiceLocator();
   }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      navigatorKey: _navigationService.navigatorKey,
       title: 'Framework',
       builder: (BuildContext context, Widget widget) => Navigator(
         onGenerateRoute: (RouteSettings settings) => MaterialPageRoute(
